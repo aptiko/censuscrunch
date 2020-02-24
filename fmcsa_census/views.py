831ae1b2b0c01e7ae71a67a1302fb5b82bf6c0b3
Antonis Christofides <antonis@antonischristofides.com>
2020-02-24 07:36:17 +0200

    Implement search by number of power units

--- fmcsa_census/views.py
+++ fmcsa_census/views.py
@@ -7,3 +7,18 @@ class SearchView(ListView):
     model = models.Carrier
     paginate_by = 100
     template_name = "fmcsa_census/search/main.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = self._filter_queryset(qs)
+        return qs
+
+    def _filter_queryset(self, qs):
+        min_power_units = self.request.GET.get("min_number_of_power_units")
+        max_power_units = self.request.GET.get("max_number_of_power_units")
+        if min_power_units:
+            qs = qs.filter(number_of_power_units__gte=min_power_units)
+        if max_power_units:
+            qs = qs.filter(number_of_power_units__lte=max_power_units)
+        return qs
+
